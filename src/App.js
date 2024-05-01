75fd3afd59ede2786c907c875f300918600760b8
Vaidant123 <vaidanttomarlk@gmail.com>
2024-05-02 00:37:34 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function XPagination() {
+  const [members, setMembers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    fetchMembers();
+  }, [currentPage]);
+
+  const fetchMembers = async () => {
+    try {
+      const response = await fetch(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setMembers(data);
+      setTotalPages(Math.ceil(data.length / 10));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      alert("Failed to fetch data");
+    }
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+  const startIndex = (currentPage - 1) * 10;
+  const endIndex = Math.min(startIndex + 10, members.length);
+
+  return (
+    <div className="container">
+      <h2>Employee Data</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {members.slice(startIndex, endIndex).map((member) => (
+            <tr key={member.id}>
+              <td>{member.id}</td>
+              <td>{member.name}</td>
+              <td>{member.email}</td>
+              <td>{member.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={handlePrevious} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span class="page">{currentPage}</span>
+        <button onClick={handleNext} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default XPagination;
